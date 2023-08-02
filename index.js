4d3184ec95f610dc0533ab9610034ba0a214b09e
Oleksii Korpan <oleksii.korpan@gmail.com>
2023-08-02 11:44:26 +0000

    fix port

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ process.env.BASIC_AUTH_PASSWORD = 'admin';
 // start webserver
 const server = require('./webserver/server');
 server.listen(process.env.port, () => {
-    console.log(`Server is running on http://127.0.0.1:${process.env.port}/`);
+    console.log(`Server is running on http://127.0.0.1:${process.env.PORT}/`);
 });
